f3aa2c70a19f9d76205b8fd99dea5b0fb9cd79e0
Gopala-kri <138243972+Gopala-kri@users.noreply.github.com>
2023-09-15 08:57:51 +0530

    myproject

---
+++ main.js
@@ -0,0 +1,18 @@
+var a= num1;
+
+var b= num2;
+
+function performAddition() {
+
+parseFloat(document.getElementById("a").value); parseFloat (document.getElementById("b").value);
+
+var sum = a + b;
+
+var audio = document.getElementById("audio");
+
+audio.play();
+
+document.getElementById("result").textContent = "The result of the addition is:
+
++ result;
+}
